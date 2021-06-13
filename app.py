6be15ac92b5393c99b9dfac9d31122896cf4c183
Ali Kamel <alik87932@gmail.com>
2021-06-13 19:57:25 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template, session,request, redirect
+
+app = Flask(__name__)
+app.secret_key = "dsdnkdso£*4492r4_2398u"
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('test_index.html')
+
+@app.route('/', methods=["POST"])
+def decision():
+    session["on"] = False
+    try:
+        if request.form["LEDon"]: session["on"] = True
+    except KeyError:
+        pass
+    return redirect('/')
+
+@app.route('/decision', methods=["GET"])
+def returnDecision():
+    print(session["on"])
+    return redirect('/')
