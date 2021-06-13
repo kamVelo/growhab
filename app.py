40b1281722333124bee7f295489e48570d322a9d
Ali Kamel <alik87932@gmail.com>
2021-06-14 00:16:42 +0100

    write attempt 3

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/', methods=["POST"])
 def decision():
-    file = open("./templates/decision.html", "w")
+    file = open("./static/text/decision.txt", "w")
     try:
         if request.form["LEDon"]: file.write("HIGH")
     except KeyError:
@@ -19,4 +19,7 @@ def decision():
 
 @app.route('/decision', methods=["GET"])
 def returnDecision():
-    return render_template('decision.html')
+    file = open ("./static/text/decision.txt", "r")
+    var = file.readlines()[0]
+    file.close()
+    return var
